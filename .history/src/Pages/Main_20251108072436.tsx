9632279d7abe6d79334abc9e353efd2675d3886c
darae1225 <darae01225@gmail.com>
2025-11-08 07:45:36 +0800

    UI updated on Filter component

---
+++ .history/src/Pages/Main_20251108072436.tsx
@@ -0,0 +1,26 @@
+import Header from "../components/header/Header";
+import Membership from "../components/membership/Membership";
+import List from "../components/list/List";
+import FlybuysLink from "./FlybuysLink";
+import { useState } from "react";
+
+export default function Main() {
+  const [items, setItems] = useState([]);
+
+  const handleAdd = (newItem) => {
+    setItems((prev) => [...prev, newItem]);
+  };
+
+  const handleDelete = (deletedItem) => {
+    setItems((prev) => prev.filter((item) => item.id !== deletedItem.id));
+  };
+
+  return (
+    <>
+      <Header onAdd={handleAdd} />
+      <Membership />
+      <List items={items} onDelete={handleDelete} />
+      <FlybuysLink />
+    </>
+  );
+}
